f871389b758971192a944b84503fc02ad3026782
NataliaMolina <molinanatalia483@gmail.com>
2019-02-12 14:00:28 +0100

    Añade atributos en clase modelo

---
+++ src/tarea4nataliamolina/Mascota.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4nataliamolina;
+
+/**
+ *
+ * @author nataliamc
+ */
+public class Mascota {
+
+    private String nombre;
+    private int edad;
+    private String raza;
+    private String color;
+    
+}
